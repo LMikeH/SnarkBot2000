d1f5efe9ce4022e29f495a6217ae48c61aac1364
LMikeH <lmichaelhuang3@gmail.com>
2018-05-05 15:19:14 -0400

    motor update 1

--- MotorControls/moter.ino
+++ MotorControls/moter.ino
@@ -4,7 +4,10 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *frontleft_motor = AFMS.getMotor(1);
+Adafruid_DCMotor *backleft_motor = AFMS.getMotor(2);
+Adafruid_DCMotor *frontright_motor = AFMS.getMotor(3);
+Adafruid_DCMotor *backright_motor = AFMS.getMotor(4);
 String motorin;
 
 void setup() {
--- MotorControls/motercontrol.py
+++ MotorControls/motercontrol.py
@@ -1,22 +1,62 @@
 import serial
+import numpy as np
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 motorstr = str.encode('-023,-255,+000,+040')
 
 
+def speedstring(factor):
+    """
+    Utility function, makes string for serial
+    from speed integer. 
+    example:
+    220 --> '+220'
+    """ 
+    if factor >= 0:
+        sign = '+'
+    else:
+        sign = '-'
+
+    if abs(factor) < 10:
+        return sign +'00'+str(factor)
+    elif abs(factor) > 99:
+        return sign + str(factor)
+    else:
+        return sign + '0' + str(factor)
+
 class RoboDrive:
+    """
+    This class defines a robot drive train object.
+    """
     def __init__():
         self.speedfl = 0 # speed front left
         self.speedbl = 0 # speed back left
         self.speedfr = 0 # speed front right
         self.speedbr = 0 # speed back right
 
-
     def speed_encode(self):
+        flstr = speedstring(self.speedfl)
+        blstr = speedstring(self.speedbl)
+        frstr = speedstring(self.speedfr)
+        brstr = speedstring(self.speedbr)
+        motorstr = '{},{},{},{}'.format(flstr,blstr,frstr,brstr)
+        return motorstr
 
+    def arcturn(self,speed,angle): #WRONG WAY!!!!!!!!!!!
+        turn_adjustment = np.cos(np.pi*angle/180.0)
+        if angle >= 0:
+            left_factor = 1
+            right_factor = int(turn_adjustment)
+        elif angle <=0:left_factor = 1
+            left_factor = int(turn_adjustment)
+            right_factor = 1
 
+        self.speedfl = speed*left_factor
+        self.speedbl = speed*left_factor
+        self.speedfr = speed*right_factor
+        self.speedbr = speed*right_factor
 
-
+##############################
 while True:
     ser.write(motorstr)
     print(ser.readline())
