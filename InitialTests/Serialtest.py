d7d5a4e15fd90788bf8dbe6a8f5bc61baa2a3022
LMikeH <lmichaelhuang3@gmail.com>
2018-07-20 16:56:36 -0400

    Motor controls setup

--- InitialTests/Serialtest.py
+++ InitialTests/Serialtest.py
@@ -1,9 +1,11 @@
 import serial
+import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-motorstr = str.encode('-023,-255,+000,+040')
+motorstr = str.encode('f123,b240')
 
 
 while True:
     ser.write(motorstr)
     print(ser.readline())
+    time.sleep(1)
--- MotorControls/moter.ino
+++ MotorControls/moter.ino
@@ -1,13 +1,12 @@
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include <utility/Adafruit_MS_PWMServoDriver.h>
+#include <math.h>
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *frontleft_motor = AFMS.getMotor(1);
-Adafruid_DCMotor *backleft_motor = AFMS.getMotor(2);
-Adafruid_DCMotor *frontright_motor = AFMS.getMotor(3);
-Adafruid_DCMotor *backright_motor = AFMS.getMotor(4);
+Adafruit_DCMotor *backleft_motor = AFMS.getMotor(2);
+Adafruit_DCMotor *frontright_motor = AFMS.getMotor(3);
+Adafruit_DCMotor *backright_motor = AFMS.getMotor(4);
 String motorin;
 
 void setup() {
@@ -15,20 +14,38 @@ void setup() {
   AFMS.begin();
 }
 
-int lfspd = 0;
-int lbspd = 0;
-int rfspd = 0;
-int rbspd = 0;
-
 void loop() {
 
-    if (Serial.available()) {
-        delay(2000);
-        continue;
-    }
-    else {
-        motor1->setSpeed(0);
-        motor1->run(RELEASE);
-    }
+    int leftspd = 0;
+    int rightspd = 0;
 
+    String speedstr;
+    speedstr = Serial.readString();
+    String leftsign = speedstr.substring(0,1);
+    String leftstr = speedstr.substring(1,4);
+    String rightsign = speedstr.substring(5,6);
+    String rightstr = speedstr.substring(6,9);
+    Serial.println(leftsign);
+
+    leftspd = leftstr.toInt();
+    rightspd = rightstr.toInt();
+
+
+    frontleft_motor->setSpeed(leftspd);
+    frontleft_motor->run(FORWARD);
+
+    backleft_motor->setSpeed(leftspd);
+    backleft_motor->run(FORWARD);
+
+    //if (leftsign == '-') {
+    //    Serial.println(leftsign);
+    //}
+
+    frontright_motor->setSpeed(rightspd);
+    frontright_motor->run(FORWARD);
+
+    backright_motor->setSpeed(rightspd);
+    backright_motor->run(FORWARD);
+
+    delay(50);
 }
