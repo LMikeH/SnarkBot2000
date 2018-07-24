e8fbaf74cefcc63ff78cdbdc5089234b619d59b3
LMikeH <lmichaelhuang3@gmail.com>
2018-07-23 21:53:53 -0400

    Timing issues...

--- snarktest.py
+++ snarktest.py
@@ -4,13 +4,12 @@ import ultrasonic.hexaultra as hu
 import time
 import serial
 import numpy as np
+start = time.time()
 
-motorser = serial.Serial('/dev/ttyACM1', 9600)
-ultraser = serial.Serial('/dev/ttyACM0', 9600)
+motorser = serial.Serial('/dev/ttyACM0', 9600)
+ultraser = serial.Serial('/dev/ttyACM1', 9600)
 
 motor = mc.RoboDrive(motorser)
-motorser.flushInput()
-ultraser.flushInput()
 
 ang = 0
 #time.sleep(1)
@@ -20,12 +19,14 @@ while True:
     print(uray)
     ang += .1
     degrees = 30*np.sin(ang)
-    print(motorser.readline())
+    #print(motorser.readline())
     if uray[0] > 20.0 and uray[1] > 20.0 and uray[2] > 20.0:
         motor.move(100.0,degrees)
     else:
         motor.move(0.0,degrees)
-    motorser.flushInput()
+    #motorser.flushInput()
     ultraser.flushInput()
-    motorser.flushOutput()
-    #time.sleep(.1)
+    #motorser.flushOutput()
+    #ultraser.flushOutput()          # added
+    #time.sleep(.0001)
+    print(time.time()-start)
